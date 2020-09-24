4afc2d49c37dd70c34d7f6c5c84ce4b21951aba1
ofekbytes <ofekbytes@gmail.com>
2020-09-24 19:47:39 +0300

    test

--- swing/src/main/java/swing/Main.java
+++ swing/src/main/java/swing/Main.java
@@ -2,20 +2,9 @@ package swing;
 
 import swing.test.MainTest;
 
-
 public class Main {
-
-	public Main() {
+	public static void main(String[] args) {
 		System.out.println("Main constructor");
 		new MainTest();
 	}
-
-	public static void main(String[] args) {
-		System.out.println("main");
-		new Main();
-		
-	}
-
 }
-
-
--- swing/src/main/java/swing/test/Test_01.java
+++ swing/src/main/java/swing/test/Test_01.java
@@ -14,67 +14,54 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Test_01 implements ActionListener {
-	
-	
 	private JFrame frame;
 	private JPanel panel;
 	private JButton button;
 	private JLabel label;
-	
 	private int count = 0;
-	
-	public Test_01()  {
-	
+
+	public Test_01() {
 		frame = new JFrame();
-		
+
 		panel = new JPanel();
-		
+
 		button = new JButton("click me");
 		button.addActionListener(this);
-			
+
 		label = new JLabel("number of clicks: 0      ");
-		
-		
-		//jpanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		//jpanel.setBorder(BorderFactory.createEmptyBorder());
-		
-		//border
+
+		// jpanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		// jpanel.setBorder(BorderFactory.createEmptyBorder());
+
+		// border
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 60, 90));
-		
-		//layout manger
+
+		// layout manger
 		panel.setLayout(new GridLayout(0, 1));
-		
-		
-		//button add
+
+		// button add
 		panel.add(button);
 		panel.add(label);
-		
-		
+
 		// frame <--- panel
 		frame.add(panel, BorderLayout.CENTER);
-		
+
 		// close action
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //frame.EXIT_ON_CLOSE
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // frame.EXIT_ON_CLOSE
+
 		// title
 		frame.setTitle("Test_01");
-		
+
 		// set the window to a match a preferred size.
 		frame.pack();
-	
+
 		// visible + focus
 		frame.setVisible(true);
-		
 	}
 
+	// // ActionListener // //
 	public void actionPerformed(ActionEvent e) {
 		count++;
-		label.setText("number of clicks: " + count + " "); 
+		label.setText("number of clicks: " + count + " ");
 	}
-
-
-	
-	
-
-
 }
