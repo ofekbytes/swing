ff27015adb988df8a0e93edc8a184b33ad1c07bc
yaron <yaron.kessler41@gmail.com>
2022-06-06 08:17:58 +0300

    test

--- swing/src/main/java/swing/test/MainTest.java
+++ swing/src/main/java/swing/test/MainTest.java
@@ -5,8 +5,8 @@ public class MainTest {
 	public MainTest() {
 		System.out.println("MainTest");
 				
-		new Test_01();
-//		new Test_02();
+//		new Test_01();
+		new Test_02();
 //		new Test_03();
 	}
 
